d5d997d44177afdf37d7fb7190d75ff48d7b81c6
Reem Alharrasi <reemalharrasi00@gmail.com>
2026-08-18 21:37:51 +0400

    Person overloaded constructor [Task 1.1]

--- SchoolManagementSystem/src/entities/Person.java
+++ SchoolManagementSystem/src/entities/Person.java
@@ -9,7 +9,6 @@ public class Person {
     private boolean activeStatus;
 
     //constructors
-
     public Person(Integer id, String firstName, String lastName, String dateOfBirth, String gender, Integer phoneNumber, String email, String address, Integer nationalID, Integer age, boolean activeStatus) {
         this.id = id;
         this.firstName = firstName;
@@ -24,4 +23,9 @@ public class Person {
         this.activeStatus = activeStatus;
     }
 
+    public Person(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
