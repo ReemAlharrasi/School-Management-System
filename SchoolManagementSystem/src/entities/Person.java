f10cd151dba99ea98fc0c8363c148396e20f9829
Reem Alharrasi <reemalharrasi00@gmail.com>
2026-08-18 21:37:07 +0400

    Person attributes + full constructor [Task 1.1]

---
+++ SchoolManagementSystem/src/entities/Person.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Person {
+    private Integer id;
+    private String firstName, lastName,dateOfBirth, gender;
+    private Integer phoneNumber;
+    private String email, address;
+    private Integer nationalID,age;
+    private boolean activeStatus;
+
+    //constructors
+
+    public Person(Integer id, String firstName, String lastName, String dateOfBirth, String gender, Integer phoneNumber, String email, String address, Integer nationalID, Integer age, boolean activeStatus) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.address = address;
+        this.nationalID = nationalID;
+        this.age = age;
+        this.activeStatus = activeStatus;
+    }
+
+}
