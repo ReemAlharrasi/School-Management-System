fa30f049ced854d960abf9d0e76787180dd2d116
Reem Alharrasi <reemalharrasi00@gmail.com>
2026-08-18 22:12:37 +0400

    Student extends Person + fields [Task 1.2]

--- SchoolManagementSystem/src/entities/Student.java
+++ SchoolManagementSystem/src/entities/Student.java
@@ -1,4 +1,37 @@
 package entities;
 
-public class Student {
+public class Student extends Person{
+    private String gradeLevel,enrollmentDate;
+    String[] enrolledSubjects, recordIDs;
+    private double feeBalance;
+    private boolean isScholarship;
+
+    //constructor
+    public Student(Integer id, String firstName, String lastName, String dateOfBirth, String gender, Integer phoneNumber, String email, String address, Integer nationalID, Integer age, boolean activeStatus, String gradeLevel, String enrollmentDate, String[] enrolledSubjects, String[] recordIDs, double feeBalance, boolean isScholarship) {
+        super(id, firstName, lastName, dateOfBirth, gender, phoneNumber, email, address, nationalID, age, activeStatus);
+        this.gradeLevel = gradeLevel;
+        this.enrollmentDate = enrollmentDate;
+        this.enrolledSubjects = enrolledSubjects;
+        this.recordIDs = recordIDs;
+        this.feeBalance = feeBalance;
+        this.isScholarship = isScholarship;
+    }
+
+    //getters
+    public String getGradeLevel() {return gradeLevel;}
+    public String getEnrollmentDate() {return enrollmentDate;}
+    public String[] getEnrolledSubjects() {return enrolledSubjects;}
+    public String[] getRecordIDs() {return recordIDs;}
+    public double getFeeBalance() {return feeBalance;}
+    public boolean isScholarship() {return isScholarship;}
+
+    //setters
+    public void setGradeLevel(String gradeLevel) {this.gradeLevel = gradeLevel;}
+    public void setEnrollmentDate(String enrollmentDate) {this.enrollmentDate = enrollmentDate;}
+    public void setEnrolledSubjects(String[] enrolledSubjects) {this.enrolledSubjects = enrolledSubjects;}
+    public void setRecordIDs(String[] recordIDs) {this.recordIDs = recordIDs;}
+    public void setFeeBalance(double feeBalance) {this.feeBalance = feeBalance;}
+    public void setScholarship(boolean scholarship) {isScholarship = scholarship;}
+
+
 }
