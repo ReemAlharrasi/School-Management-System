f667291acde0cf14da71e8da32df28dd70f192e7
Reem Alharrasi <reemalharrasi00@gmail.com>
2026-08-18 21:40:06 +0400

    Person getters/setters [Task 1.1]

--- SchoolManagementSystem/src/entities/Person.java
+++ SchoolManagementSystem/src/entities/Person.java
@@ -28,4 +28,32 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    //getters
+    public Integer getId() {return id;}
+    public String getFirstName() {return firstName;}
+    public String getLastName() {return lastName;}
+    public String getDateOfBirth() {return dateOfBirth;}
+    public String getGender() {return gender;}
+    public Integer getPhoneNumber() {return phoneNumber;}
+    public String getEmail() {return email;}
+    public String getAddress() {return address;}
+    public Integer getNationalID() {return nationalID;}
+    public Integer getAge() {return age;}
+    public boolean isActiveStatus() {return activeStatus;}
+
+    //setters
+    public void setId(Integer id) {this.id = id;}
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public void setLastName(String lastName) {this.lastName = lastName;}
+    public void setDateOfBirth(String dateOfBirth) {this.dateOfBirth = dateOfBirth;}
+    public void setGender(String gender) {this.gender = gender;}
+    public void setPhoneNumber(Integer phoneNumber) {this.phoneNumber = phoneNumber;}
+    public void setEmail(String email) {this.email = email;}
+    public void setAddress(String address) {this.address = address;}
+    public void setNationalID(Integer nationalID) {this.nationalID = nationalID;}
+    public void setAge(Integer age) {this.age = age;}
+    public void setActiveStatus(boolean activeStatus) {this.activeStatus = activeStatus;}
+    
+
 }
